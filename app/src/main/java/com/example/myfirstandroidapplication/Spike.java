0a1d8a8976df66b596b777f9ee2659ec8e529e43
RoyFerguson0 <roy.ferguson.jr@outlook.com>
2023-12-20 13:48:19 +0000

    Roy - 20/12/2023 - 13:47 Added Images for Game that will be needed. Created the Spike Class to get the Spike (Falling Object) Created the explosion Class to get the explosion Created the MainActivity menu (Start Screen)

---
+++ app/src/main/java/com/example/myfirstandroidapplication/Spike.java
@@ -0,0 +1,49 @@
+package com.example.myfirstandroidapplication;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
+
+public class Spike {
+    // For holding spike images
+    Bitmap spike[] = new Bitmap[3];
+
+    // Declaring Varialbes
+    int spikeFrame = 0;
+    int spikeX, spikeY, spikeVelocity;
+    Random random;
+
+    public  Spike(Context context){
+        // Initialise Spike images in the Array (Bitmap)
+        spike[0] = BitmapFactory.decodeResource(context.getResources(), R.drawable.bomb);
+        spike[1] = BitmapFactory.decodeResource(context.getResources(), R.drawable.dynamite);
+        spike[2] = BitmapFactory.decodeResource(context.getResources(), R.drawable.nuclearbomb);
+
+        // Intialising Random
+        random = new Random();
+
+        // Used to reset spikeX, spikeY and spikeVelocity
+        resetPosition();
+    }
+
+
+    public Bitmap getSpike(int spikeFrame){
+        return spike[spikeFrame];
+    }
+
+    public int getSpikeWidth(){
+        return spike[0].getWidth();
+    }
+
+    public int getSpikeHeight(){
+        return spike[0].getHeight();
+    }
+    public void resetPosition(){
+        // Intialise spike X,Y,V with Randoms Spike
+        spikeX = random.nextInt()(GameView.dWidth - getSpikeWidth());
+        spikeY = -200 + random.nextInt(600) * -1;
+        spikeVelocity = 35 + random.nextInt(16);
+    }
+}
