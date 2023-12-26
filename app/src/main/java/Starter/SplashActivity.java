131238dd7bf7a4412892b76308415090f2f24267
Roy Ferguson <50004216@belfastmet.ac.uk>
2023-12-26 02:23:38 +0000

    Roy - 26/12/2023 - 02:23
    - Slash Screen Design Complete
    - Added in Animations plus a progress bar

--- app/src/main/java/Starter/SplashActivity.java
+++ app/src/main/java/Starter/SplashActivity.java
@@ -5,25 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.myfirstandroidapplication.R;
 
 public class SplashActivity extends AppCompatActivity {
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, developer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // initiate progress bar
+        final ProgressBar progBar = findViewById(R.id.progLoadSplash);
+
+        // Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image = findViewById(R.id.imgMobileMadnessLogo);
+        logo = findViewById(R.id.lblCompanyLogo);
+        developer = findViewById(R.id.lblDeveloperName);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        developer.setAnimation(bottomAnim);
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 Intent intent = new Intent(SplashActivity.this, MenuActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 4000);
+        }, 5000);
 
 
     }
